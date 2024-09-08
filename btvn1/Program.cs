982480742f9c9da17b9e7e9646b258cc9e8986e6
PV <PV@LAPTOPCUALANMOO>
2024-09-08 15:06:43 +0700

    Add project files.

---
+++ btvn1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace btvn1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
