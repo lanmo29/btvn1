4cc497ea630185ecdd1a61cc848256785ce4e875
lanmo29 <hungmo29072003@gmail.com>
2024-09-10 00:14:23 +0700

    Add files via upload

---
+++ Winform_lib/Winform_lib/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using lib_phepnhan;
+
+namespace Winform_lib
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float a, b;
+            String so1 = tb_nhapso1.Text;
+            String so2 = tb_nhapso2.Text;
+            if (!float.TryParse(so1, out a))
+            {
+                MessageBox.Show("Giá trị của a không hợp lệ.");
+                return;
+            }
+            if (!float.TryParse(so2, out b))
+            {
+                MessageBox.Show("Giá trị của b không hợp lệ.");
+                return;
+            }
+            float kq = phepnhan.Phepnhan(a,b);
+            String kq2 = kq.ToString();
+            kq3.Text = "Gia tri cua phep nhan la: "+kq2;
+        }
+    }
+}
---
+++ Winform_lib/Winform_lib/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Winform_lib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Winform_lib")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("94c9df8f-941c-409b-b3a6-7450ffdfc146")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
