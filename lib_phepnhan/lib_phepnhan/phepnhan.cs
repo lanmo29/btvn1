7b28ae9296fc0fcd16044e8534882993ec593d6d
lanmo29 <hungmo29072003@gmail.com>
2024-09-10 00:17:34 +0700

    Add files via upload

---
+++ lib_phepnhan/lib_phepnhan/phepnhan.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lib_phepnhan
+{
+    public class phepnhan
+    {
+        public static float Phepnhan(float a, float b)
+        {
+            return a * b;
+        }
+    }
+}
