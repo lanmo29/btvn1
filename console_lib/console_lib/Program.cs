645540a090538403f9c055484fe1e4a5a026dd8a
lanmo29 <hungmo29072003@gmail.com>
2024-09-10 00:15:28 +0700

    Add files via upload

---
+++ console_lib/console_lib/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lib_phepnhan;
+namespace console_lib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float kq = phepnhan.Phepnhan(3, 7);
+            Console.WriteLine("Ket qua phep tinh la:{0}",kq);
+            Console.ReadLine();
+        }
+    }
+}
