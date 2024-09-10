76784c9d950382cadce0398176ba5d350aff9ae0
lanmo29 <hungmo29072003@gmail.com>
2024-09-10 22:03:07 +0700

    Add files via upload

---
+++ WebApplication2/JavaScript.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    function sendData() {
+        var a = $('#txtA').val();
+        var b = $('#txtB').val();
+
+        if (isNaN(a) || isNaN(b)) {
+            alert('Vui lòng nhập giá trị hợp lệ cho đáy lớn , đáy nhỏ và chiều cao.');
+            return;
+        }
+
+        var dataToSend = {
+            soa: a,
+            sob: b,
+        };
+
+        $.ajax({
+            url: "WebForm1.aspx",
+            type: "POST",
+            data: JSON.stringify(dataToSend),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (response) {
+                $('#lblResult').text('Diện tích hình thang là: ' + response);
+            },
+            error: function (xhr, status, error) {
+                $('#lblResult').text('Có lỗi xảy ra: ' + xhr.responseText);
+            }
+        });
+    }
+
+    $("#btnCalculate").click(function () {
+        sendData();
+    });
+});
