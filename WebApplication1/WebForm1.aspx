5cdaf01db09b510e7c6a16f37c6fba1bd5544d06
lanmo29 <hungmo29072003@gmail.com>
2024-09-10 22:02:34 +0700

    Add files via upload

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Tính tích hai số</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Tính tích hai số</h2>
+
+            <label for="so_a">Nhập giá trị a: </label>
+            <asp:TextBox ID="so_a" runat="server"></asp:TextBox>
+            <br /><br />
+
+            <label for="so_b">Nhập giá trị b: </label>
+            <asp:TextBox ID="so_b" runat="server"></asp:TextBox>
+            <br /><br />
+
+
+            <asp:Button ID="btnCalculate" runat="server" Text="Tính Toán" OnClick="btnCalculate_Click" />
+            <br /><br />
+
+            <asp:Label ID="lblResult" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ClassLibrary1;
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double a, b, h;
+            string errorMessage = "";
+
+            if (!double.TryParse(so_a.Text, out a) || a <= 0)
+            {
+                errorMessage += "Đáy lớn (a) phải là số dương. ";
+            }
+            if (!double.TryParse(so_b.Text, out b) || b <= 0)
+            {
+                errorMessage += "Đáy nhỏ (b) phải là số dương. ";
+            }
+
+            if (errorMessage != "")
+            {
+                lblResult.Text = "Lỗi: " + errorMessage;
+            }
+            else
+            {
+                double area = Class1.tich(a, b);
+                lblResult.Text = $"Diện tích hình thang là: {area}";
+            }
+        }
+    }
+}
