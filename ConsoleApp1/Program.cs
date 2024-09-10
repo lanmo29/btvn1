a67b698076dd5c8156dc356e0ed7454216d96d23
lanmo29 <hungmo29072003@gmail.com>
2024-09-10 22:01:31 +0700

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ClassLibrary1;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double a , b ;
+                while (true)
+                {
+                    Console.Write("Nhap a : ");
+                    if (double.TryParse(Console.ReadLine(), out a) && a > 0)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Nhap sai roi , nhap lai!");
+                }
+
+                while (true)
+                {
+                    Console.Write("Nhap b: ");
+                    if (double.TryParse(Console.ReadLine(), out b) && b > 0)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Nhap sai roi , nhap lai!");
+                }
+                double tichso = Class1.tich(a, b);
+                Console.WriteLine("Tich 2 so la :" + tichso);
+
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Lỗi: " + ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
